a3fc871854bf18d60ad5eca36525a5cd170fc032
lxf1992521 <lxf1992521@gmail.com>
2023-10-31 10:39:10 +0800

    xenial-backports

--- xenial-backports/start.sh
+++ xenial-backports/start.sh
@@ -5,7 +5,7 @@ cd -- "$(dirname -- "$0")" || exit 1
 docker run -it --rm --network=host -w /root --name=ubuntu-build \
            -v /data/ubuntu/packages/builder/data/:/data/ \
            -v /data/ubuntu/packages/builder/debhelper/:/debhelper/ \
-           -v xenial-backports-apt-lists:/var/lib/apt/lists/ \
            -v xenial-backports-apt-cache:/var/cache/apt/archives/ \
+           -v xenial-backports-apt-lists:/var/lib/apt/lists/ \
            -v $PWD/bashrc:/root/.bashrc:ro \
            ubuntu-build
